a6a2cfaaebaced12502e8dca03f54fd70211962f
Retr0327 <philcoke35@gmail.com>
2023-02-08 14:23:42 +0800

    Add get_env for env variable retrieval

---
+++ bot/config/env.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+from typing import Optional, Dict, Union
+from dotenv import load_dotenv
+
+ENV_PATH = Path("__file__").resolve().parent / ".env"
+load_dotenv(ENV_PATH)
+
+
+def get_env(field: Optional[str] = None) -> Union[Dict[str, str], str]:
+    """The get_env function gets the environment variables."""
+    env_dict = {"token": os.environ.get("TOKEN"), "guild": int(os.environ.get("GUILD"))}
+    if field is None:
+        return env_dict
+
+    return env_dict[field]
