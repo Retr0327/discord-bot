818069bd614dbb6acaf3235c86c6dd9c03c296af
Retr0327 <philcoke35@gmail.com>
2023-02-08 20:36:36 +0800

    Implement text channel deletion command

--- bot/controllers/channel/__init__.py
+++ bot/controllers/channel/__init__.py
@@ -1,4 +1,9 @@
 from .create import handle_create_channel
+from .delete import handle_delete_channel
 from .clear import handle_clear_all_messages
 
-__all__ = ["handle_create_channel", "handle_clear_all_messages"]
+__all__ = [
+    "handle_create_channel",
+    "handle_delete_channel",
+    "handle_clear_all_messages",
+]
