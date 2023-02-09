fc3319e5f5ec64341a11e72e791bce92f9fcc73f
Retr0327 <philcoke35@gmail.com>
2023-02-09 22:50:30 +0800

    Reframe DiscordClient

--- bot/models/discord.py
+++ bot/models/discord.py
@@ -1,14 +1,26 @@
-from discord import app_commands, Intents, Client
+from log import setup_logger
+from config import BOT_PREFIX, get_env
+from discord.ext.commands import Bot, when_mentioned_or
+from discord import Intents, Activity, ActivityType, Object
+
+logger = setup_logger(__name__)
 
 
-class DiscordClient(Client):
-    """
-    The Bot object represents a client connection that connects to Discord.
-    """
-
+class DiscordClient(Bot):
     def __init__(self) -> None:
-        intents = Intents.default().all()
+        intents = Intents.default()
         intents.message_content = True
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
-        self.synced = False
+        intents.members = True
+        activity = Activity(name=f"{BOT_PREFIX}help", type=ActivityType.listening)
+        super().__init__(
+            command_prefix=when_mentioned_or(BOT_PREFIX),
+            intents=intents,
+            activity=activity,
+        )
+
+    async def on_ready(self) -> None:
+        logger.info("⚡️[bot] Ready to accept connections")
+
+    async def setup_hook(self) -> None:
+        await self.tree.sync(guild=Object(id=get_env("guild")))
+        logger.info("⚡️[bot] Slash commands synced!")
