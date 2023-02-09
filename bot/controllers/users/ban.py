03d25ba0e13c930810dc3a436a6de72d4afebf3f
Retr0327 <philcoke35@gmail.com>
2023-02-09 13:12:26 +0800

    Reframe ban

--- bot/controllers/users/ban.py
+++ bot/controllers/users/ban.py
@@ -2,6 +2,11 @@ from typing import Optional
 from models import DiscordClient
 from discord import Object, Member, Interaction
 
+template = {
+    "member": "Dear {}, you have been banned from {}\nReason: {}",
+    "channel": "**{}** has been banned!\nReason: {}",
+}
+
 
 def handle_ban(client: DiscordClient, guild: int):
     """The handle_ban function bans a user.
@@ -21,7 +26,15 @@ def handle_ban(client: DiscordClient, guild: int):
     async def ban(
         interaction: Interaction, member: Member, reason: Optional[str] = None
     ):
-        await member.ban(reason=reason)
-        await interaction.response.send_message(f":shushing_face: Be quiet **{member}**")
+        member_msg = template["member"].format(
+            member.name,
+            interaction.guild.name,
+            "Not Specified" if reason is None else reason,
+        )
+        channel_msg = template["channel"].format(member.name, "Not Specified")
+
+        await member.send(member_msg)
+        await interaction.response.send_message(channel_msg)
+        await member.ban()
 
     return ban
