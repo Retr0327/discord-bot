fb6a15643e0a4e9fc0c290ffd929187390cf8ed0
Retr0327 <philcoke35@gmail.com>
2023-02-09 11:08:36 +0800

    Add production mode check using `PY_ENV` environment variable

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,5 +1,4 @@
-from .env import get_env
+from .env import get_env, is_production
 from .config import BOT_PREFIX, LOG_PATH
 
-
-__all__ = ["get_env", "BOT_PREFIX", "LOG_PATH"]
+__all__ = ["get_env", "is_production", "BOT_PREFIX", "LOG_PATH"]
--- bot/config/env.py
+++ bot/config/env.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 ENV_PATH = Path("__file__").resolve().parent / ".env"
 load_dotenv(ENV_PATH)
 
+is_production = os.environ.get("PY_ENV") == 'production'
 
 def get_env(field: Optional[str] = None) -> Union[Dict[str, str], str]:
     """The get_env function gets the environment variables."""
     env_dict = {
         "token": os.environ.get("TOKEN"),
         "guild": int(os.environ.get("GUILD")),
-        "PY_ENV": os.environ.get("PY_ENV"),
     }
     if field is None:
         return env_dict
