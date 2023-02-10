7b939d147b6b68a8a43c456c7a09608a5638fbe2
Retr0327 <philcoke35@gmail.com>
2023-02-10 20:28:50 +0800

    Rename `schedule_message` to `create_scheduler`

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,10 +1,10 @@
-from .create import schedule_message
+from .create import create_scheduler
 from .delete import delete_scheduler
 from .read import get_scheduler, get_scheduler_by_cid
 
 __all__ = [
-    "get_scheduler",
-    "schedule_message",
+    "create_scheduler",
     "delete_scheduler",
+    "get_scheduler",
     "get_scheduler_by_cid",
 ]
--- bot/services/create.py
+++ bot/services/create.py
@@ -2,8 +2,8 @@ from typing import List
 from models.sql import query
 
 
-def schedule_message(data: List[str]) -> None:
-    """The schedule_message function insert data to the table schedule_message.
+def create_scheduler(data: List[str]) -> None:
+    """The create_scheduler function writes data to the table schedule_message.
 
     Args:
         data (list): a list of values
@@ -12,9 +12,8 @@ def schedule_message(data: List[str]) -> None:
     INSERT INTO scheduler (
         message,
         cid,
-        channel_name,
         next_event_time
     )
-    VALUES (?, ?, ?, ?);
+    VALUES (?, ?, ?);
     """
     query(sql_raw, data)
