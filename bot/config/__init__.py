2bbbe3398af51b57b93caa713abee76189b72a4f
Retr0327 <philcoke35@gmail.com>
2023-02-10 08:57:47 +0800

    Export `TOKEN` and `GUILD`

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,4 +1,10 @@
-from .env import get_env, is_production
 from .config import BOT_PREFIX, LOG_PATH
+from .env import is_production, TOKEN, GUILD
 
-__all__ = ["get_env", "is_production", "BOT_PREFIX", "LOG_PATH"]
+__all__ = [
+    "BOT_PREFIX",
+    "LOG_PATH",
+    "is_production",
+    "TOKEN",
+    "GUILD",
+]
