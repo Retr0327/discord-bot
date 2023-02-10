650a66e634d61bc24bb0c0a3e99378f9eace5f0c
Retr0327 <philcoke35@gmail.com>
2023-02-10 09:21:16 +0800

    Load extensions from `commands.setup`

--- bot/bot.py
+++ bot/bot.py
@@ -1,24 +1,12 @@
 import asyncio
-from config import get_env
+from config import TOKEN
 from models import DiscordClient
-
-TOKEN = get_env("token")
-
-command_plugins = [
-    "commands.users.ban",
-    "commands.users.echo",
-    "commands.channel.clear",
-    "commands.channel.create",
-    "commands.channel.delete",
-]
 
 
 async def run_discord_bot():
     client = DiscordClient()
     async with client:
-        await asyncio.gather(
-            *[client.load_extension(plugin) for plugin in command_plugins]
-        )
+        await client.load_extension("commands.setup")
         await client.start(token=TOKEN)
 
 
