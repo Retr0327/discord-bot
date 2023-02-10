e50b6de487fec4686c9ec026cd1f5caa70a89217
Retr0327 <philcoke35@gmail.com>
2023-02-10 08:57:30 +0800

    Remove unecessary env variables extraction

--- bot/config/env.py
+++ bot/config/env.py
@@ -1,20 +1,11 @@
 import os
 from pathlib import Path
-from typing import Optional, Dict, Union
 from dotenv import load_dotenv
 
 ENV_PATH = Path("__file__").resolve().parent / ".env"
 load_dotenv(ENV_PATH)
 
-is_production = os.environ.get("PY_ENV") == 'production'
+is_production = os.environ.get("PY_ENV") == "production"
 
-def get_env(field: Optional[str] = None) -> Union[Dict[str, str], str]:
-    """The get_env function gets the environment variables."""
-    env_dict = {
-        "token": os.environ.get("TOKEN"),
-        "guild": int(os.environ.get("GUILD")),
-    }
-    if field is None:
-        return env_dict
-
-    return env_dict[field]
+TOKEN = os.environ.get("TOKEN")
+GUILD = os.environ.get("GUILD")
