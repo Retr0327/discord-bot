f11fbb597f05dfc989033d9adbfe53757bbbf73f
Retr0327 <philcoke35@gmail.com>
2023-02-10 17:07:38 +0800

    Implement scheduler retrieval based on channel id

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,4 +1,10 @@
 from .create import schedule_message
 from .delete import delete_scheduler
+from .read import get_scheduler, get_scheduler_by_cid
 
-__all__ = ["schedule_message", "delete_scheduler"]
+__all__ = [
+    "get_scheduler",
+    "schedule_message",
+    "delete_scheduler",
+    "get_scheduler_by_cid",
+]
---
+++ bot/services/read.py
@@ -0,0 +1,11 @@
+from models.sql import query
+
+
+def get_scheduler():
+    sql_row = "SELECT * FROM scheduler"
+    return query(sql_row)
+
+
+def get_scheduler_by_cid(cid: int):
+    sql_row = "SELECT * FROM scheduler WHERE cid=?;"
+    return query(sql_row, [cid])
