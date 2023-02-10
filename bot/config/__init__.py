05e490330aadc0a28f2edcb1f9c3e7e880ad97a8
Retr0327 <philcoke35@gmail.com>
2023-02-10 10:02:51 +0800

    Export database directory path

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,7 +1,10 @@
+from .sql import DB_PATH, make_db_dir
 from .config import BOT_PREFIX, LOG_PATH
 from .env import is_production, TOKEN, GUILD
 
 __all__ = [
+    "DB_PATH",
+    "make_db_dir",
     "BOT_PREFIX",
     "LOG_PATH",
     "is_production",
