75c44b646dff189457e42351cf5d7c1b95a1af62
Retr0327 <philcoke35@gmail.com>
2023-02-11 16:56:23 +0800

    Change `cid` position

--- bot/models/sql/table.py
+++ bot/models/sql/table.py
@@ -1,8 +1,8 @@
 SCHEDULER = (
     "CREATE TABLE scheduler ("
     "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-    "message VARCHAR(1000) NOT NULL,"
     "cid DECIMAL(22,0) NOT NULL,"
+    "message VARCHAR(1000) NOT NULL,"
     "next_event_time INTEGER"
     ");"
 )
