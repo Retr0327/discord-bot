fbcee6afd9af118c43de50f006e53418c5abee95
Retr0327 <philcoke35@gmail.com>
2023-02-11 15:37:45 +0800

    Update type hint

--- bot/services/read.py
+++ bot/services/read.py
@@ -1,11 +1,24 @@
+from typing import List, Union
 from models.sql import query
 
 
-def get_schedulers():
+def get_schedulers() -> List[Union[tuple, None]]:
+    """The get_schedulers function get all the rows from the table `scheduler`.
+
+    Returns:
+        a list of tuples if the table is not empty, an empty list otherwise
+    """
     sql_row = "SELECT * FROM scheduler"
     return query(sql_row)
 
 
-def get_scheduler_by_cid(cid: int):
+def get_scheduler_by_cid(cid: int) -> List[Union[tuple, None]]:
+    """The get_scheduler_by_cid function get the row based on channel id.
+
+    Args:
+        cid (int): the channel id
+    Returns:
+        a list of tuples if the table is not empty, an empty list otherwise
+    """
     sql_row = "SELECT * FROM scheduler WHERE cid=?;"
     return query(sql_row, [cid])
