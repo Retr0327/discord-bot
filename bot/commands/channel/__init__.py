4c50b55fa7d47f741c755661c785a41f6e08e73d
Retr0327 <philcoke35@gmail.com>
2023-02-11 17:54:12 +0800

    Add TextChannelBrodcaster

--- bot/commands/channel/__init__.py
+++ bot/commands/channel/__init__.py
@@ -2,10 +2,12 @@ from .clear import ChatHistoryClearer
 from .create import TextChannelCreator
 from .delete import TextChannelRemover
 from .send import ChannelMessageSender
+from .brodcast import TextChannelBrodcaster
 
 __all__ = [
     "ChatHistoryClearer",
     "TextChannelCreator",
     "TextChannelRemover",
     "ChannelMessageSender",
+    "TextChannelBrodcaster",
 ]
