7b207681b578a7fa46158baacf2f7c9d8ae8f892
Retr0327 <philcoke35@gmail.com>
2023-02-11 19:18:25 +0800

    Change `next_event_time` type from INTEGER to REAL

--- bot/models/sql/table.py
+++ bot/models/sql/table.py
@@ -3,6 +3,6 @@ SCHEDULER = (
     "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
     "cid DECIMAL(22,0) NOT NULL,"
     "message VARCHAR(1000) NOT NULL,"
-    "next_event_time INTEGER"
+    "next_event_time REAL"
     ");"
 )
